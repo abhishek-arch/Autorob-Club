dbe2fba30dcc9de18b70eb27cfa4097d3fbbb0a3
abhishek-arch <as9044192702@gmail.com>
2025-06-23 13:58:17 +0530

    ss

--- Backend/app.js
+++ Backend/app.js
@@ -10,7 +10,7 @@ dotenv.config()
 const connectDB = require("./db/db")
 connectDB()
 // app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
-app.use(cors({ origin: 'https://abhishek-arch.github.io/Autorob-Club/', credentials: true }))
+app.use(cors({ origin: 'https://autorob-club.vercel.app/', credentials: true }))
 app.use(express.static(path.join(__dirname,"public")))
 console.log(path.__dirname)
 app.use(express.json({ limit: '10mb' })); // or more
