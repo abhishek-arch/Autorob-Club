39508f7c069d0b58dc9f45e0af08858e75385641
abhishek-arch <as9044192702@gmail.com>
2025-06-23 12:06:43 +0530

    ll

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,13 +1,28 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//    base: './',
+//   // base: '/Autorob-Club', // Change `base: '/'` to `base: '/src'`
+//   resolve: {
+//     alias: {
+//       '@': '/src' // Change `path.resolve(__dirname, 'src')` to `'/src'`
+//     }
+//   }
+// });
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-   base: '/',
-  // base: '/Autorob-Club', // Change `base: '/'` to `base: '/src'`
+  base: './',
+  build: {
+    outDir: 'dist',
+  },
   resolve: {
     alias: {
-      '@': '/src' // Change `path.resolve(__dirname, 'src')` to `'/src'`
-    }
-  }
+      // optional aliases if needed
+    },
+  },
 });
