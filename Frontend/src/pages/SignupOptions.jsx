07e1d54753d56c13f0c1a85da0d4337f6fdc1aa7
abhishek-arch <as9044192702@gmail.com>
2025-07-09 14:41:12 +0530

    done

--- Frontend/src/pages/SignupOptions.jsx
+++ Frontend/src/pages/SignupOptions.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import { Link, useNavigate } from "react-router-dom";
+import AutoRoblogo from "../assets/images/Autoroblogo.png";
 
 const SignupOptions = () => {
   return (
-    <div>
+    <div className=''>
+      <div className='flex items-center justify-self-center'>
+          <img
+                      className="w-52 mb-2 invert"
+                      src={AutoRoblogo}
+                      alt=""
+                      srcset=""
+                    />
+                    <h1 className='text-3xl font-bold'>AUTOROB CLUB</h1>
+      </div>
 
        <Link
           to="/Autorob-Club/AdminSignup"
